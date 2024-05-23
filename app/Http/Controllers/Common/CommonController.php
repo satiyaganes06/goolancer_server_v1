9a7866b21af69716374e62fbfb98c072c3064245
sG <73154081+satiyaganes06@users.noreply.github.com>
2024-05-23 14:28:47 +0800

    push image error 3

--- app/Http/Controllers/Common/CommonController.php
+++ app/Http/Controllers/Common/CommonController.php
@@ -14,7 +14,7 @@ class CommonController extends BaseController
 {
     public function imageViewer($filepath)
     {
-    //    dd($this->decode_data($filepath));
+        //    dd($this->decode_data($filepath));
         $path = storage_path($this->decode_data($filepath));
         $contents = file_get_contents($path);
         $mime = mime_content_type($path);
@@ -30,35 +30,33 @@ class CommonController extends BaseController
             $path1 = storage_path('app/uploads/images/CertificateDocument/504708-200.png');
             $contents1 = file_get_contents($path1);
             $mime1 = mime_content_type($path1);
-            
+
             return Response::make($contents1, 200, [
                 'Content-Type' => $mime1,
                 'Content-Disposition' => 'inline', // This header indicates to display the content inline (in the browser)
             ]);
         }
-
     }
 
 
     public function displayImage($app, $uploads, $folder, $category, $filename)
     {
-        $path = storage_path($app. '/' .$uploads . '/' . $folder. '/' .$category. '/' .$filename);
+        $path = storage_path($app . '/' . $uploads . '/' . $folder . '/' . $category . '/' . $filename);
         $contents = file_get_contents($path);
         $mime = mime_content_type($path);
-//app/uploads/images/JobResultFile/welcome2.png
-        try {
-            if (file_exists($path)) {
-                // return response()->file($path);
-                return Response::make($contents, 200, [
-                    'Content-Type' => $mime,
-                    'Content-Disposition' => 'inline', // This header indicates to display the content inline (in the browser)
-                ]);
-            } 
-        } catch (\Throwable $e) {
+        //app/uploads/images/JobResultFile/welcome2.png
+
+        if (file_exists($path)) {
+            // return response()->file($path);
+            return Response::make($contents, 200, [
+                'Content-Type' => $mime,
+                'Content-Disposition' => 'inline', // This header indicates to display the content inline (in the browser)
+            ]);
+        } else {
             $path1 = storage_path('app/uploads/images/CertificateDocument/504708-200.png');
             $contents1 = file_get_contents($path1);
             $mime1 = mime_content_type($path1);
-            
+
             return Response::make($contents1, 200, [
                 'Content-Type' => $mime1,
                 'Content-Disposition' => 'inline', // This header indicates to display the content inline (in the browser)
