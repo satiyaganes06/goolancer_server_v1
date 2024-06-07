cd04a318744139c846549a709924b0cfe28643f1
sG <73154081+satiyaganes06@users.noreply.github.com>
2024-06-07 23:43:17 +0800

    chore: Add downloadFile API endpoint to CommonController and update routes

--- app/Http/Controllers/Common/CommonController.php
+++ app/Http/Controllers/Common/CommonController.php
@@ -38,6 +38,33 @@ class CommonController extends BaseController
         }
     }
 
+    public function downloadFile($filepath)
+    {
+        //    dd($this->decode_data($filepath));
+        $path = storage_path($this->decode_data($filepath));
+        $contents = file_get_contents($path);
+        $mime = mime_content_type($path);
+
+        if (file_exists($path)) {
+            // return response()->file($path);
+            return Response::make($contents, 200, [
+                'Content-Type' => $mime,
+                'Content-Disposition' => 'attachment', // This header indicates to download the file
+            ]);
+        } else {
+            $path1 = storage_path('app/uploads/images/CertificateDocument/504708-200.png');
+            $contents1 = file_get_contents($path1);
+            $mime1 = mime_content_type($path1);
+
+            return Response::make($contents1, 200, [
+                'Content-Type' => $mime1,
+                'Content-Disposition' => 'attachment', // This header indicates to download the file
+            ]);
+        }
+    }
+
+
+
 
     public function displayImage($app, $uploads, $folder, $category, $filename)
     {
